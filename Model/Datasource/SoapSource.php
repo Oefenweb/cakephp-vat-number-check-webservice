1b29e49379cd2cddcfcec477752964805706d03f
Mischa ter Smitten <mischa@tersmitten.nl>
2020-09-29 11:19:40 +0200

    Fix failing tests?

--- Model/Datasource/SoapSource.php
+++ Model/Datasource/SoapSource.php
@@ -58,7 +58,7 @@ class SoapSource extends DataSource {
 		parent::__construct($config);
 
 		$this->connected = $this->connect();
-		$this->originalDefaultSocketTimeout = ini_get('default_socket_timeout') ?: 60;
+		$this->originalDefaultSocketTimeout = ini_get('default_socket_timeout');
 	}
 
 /**
